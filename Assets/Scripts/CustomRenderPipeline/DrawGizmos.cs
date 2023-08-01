84a28cf576adcc563b7c2085505ada59bb9efb70
Lubov Sokolova <falke.liebe@gmail.com>
2023-08-01 13:22:58 +0300

    add Lesson8

---
+++ Assets/Scripts/CustomRenderPipeline/DrawGizmos.cs
@@ -0,0 +1,25 @@
+﻿using UnityEditor;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+partial class CameraRenderer
+{
+    partial void DrawGizmos();
+    partial void DrawGizmos()
+    {
+        if (!Handles.ShouldRenderGizmos())
+        {
+            return;
+
+            _renderContext.DrawGizmos(_camera, GizmoSubset.PreImageEffects);
+            _renderContext.DrawGizmos(_camera, GizmoSubset.PostImageEffects);
+        }
+    }
+    partial void UIGO();
+
+    partial void UIGO()
+    {
+        if (_camera.cameraType == CameraType.SceneView)
+            ScriptableRenderContext.EmitWorldGeometryForSceneView(_camera);
+    }
+}
